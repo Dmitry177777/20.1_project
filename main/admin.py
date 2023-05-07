325915bf609b853d5e66741065eb02015f5c0ca6
Dmitry <k1791@mail.ru>
2023-05-07 11:24:30 +0500

    task3/lesson 20.1 Админка

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from main.models import Product, Category
+
 # Register your models here.
+# admin.site.register(Product)
+# admin.site.register(Category)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'description','product_category',  )
+    search_fields = ('product_name', 'description',)
+    list_filter = ('is_active', )
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('product_category', 'description', )
+    search_fields = ('product_category', 'description',)
+    list_filter = ('is_active', )
